a0fec9db44669c01870de56752de352d41f12dc8
LeoCristobal <leocristobal108@gmail.com>
2026-05-25 14:57:19 +0800

    testing

---
+++ app/schemas/auth.ts
@@ -0,0 +1,6 @@
+import z from "zod";
+export const registerSchema = z.object({
+  name: z.string().min(2).max(256),
+  email: z.email(),
+  password: z.string().min(8).max(256),
+});
---
+++ components/web/navbar.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+import { buttonVariants } from "../ui/button";
+import { ThemeToggle } from "./theme-toggle";
+
+export function Navbar() {
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <div className="flex items-center gap-8">
+        <Link href={"/"}>
+          <h1 className="text-3xl font-bold">
+            Next<span className="text-red-500">Pro</span>
+          </h1>
+        </Link>
+
+        <div className="flex items-center gap-2">
+          <Link className={buttonVariants({ variant: "ghost" })} href={"/"}>
+            Home
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href={"/blog"}>
+            Blog
+          </Link>
+          <Link
+            className={buttonVariants({ variant: "ghost" })}
+            href={"/create"}
+          >
+            Create
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        <Link className={buttonVariants()} href={"/auth/register"}>
+          Register
+        </Link>
+        <Link
+          className={buttonVariants({ variant: "outline" })}
+          href={"/auth/login"}
+        >
+          Login
+        </Link>
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
+}
